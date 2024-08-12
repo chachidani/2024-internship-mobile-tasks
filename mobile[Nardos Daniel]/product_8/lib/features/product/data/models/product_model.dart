5f16fc99066bbcae6f2e2fe19b9d696eb8b7677f
chachidani <141312783+chachidani@users.noreply.github.com>
2024-08-12 22:04:07 +0300

    feat: repository implementation

--- mobile[Nardos Daniel]/product_8/lib/features/product/data/models/product_model.dart
+++ mobile[Nardos Daniel]/product_8/lib/features/product/data/models/product_model.dart
@@ -7,26 +7,24 @@ class ProductModel extends Product {
     required String description,
     required double price,
     required String imageUrl,
-    
   }) : super(
           id: id,
           name: name,
           description: description,
           price: price,
           imageUrl: imageUrl,
-          
         );
 
   factory ProductModel.fromJson(Map<String, dynamic> json) {
-    try {return  ProductModel(
-      id: json['id'],
-      name: json['name'],
-      description: json['description'],
-      price: json['price'],
-      imageUrl: json['imageUrl'],
-     
-    );} catch (e) {
-    
+    try {
+      return ProductModel(
+        id: json['id'],
+        name: json['name'],
+        description: json['description'],
+        price: json['price'],
+        imageUrl: json['imageUrl'],
+      );
+    } catch (e) {
       throw Exception('Error in ProductModel.fromJson: $e');
     }
   }
@@ -38,7 +36,30 @@ class ProductModel extends Product {
       'description': description,
       'price': price,
       'imageUrl': imageUrl,
-     
     };
+  }
+
+  static Product toEntity(ProductModel productModel) {
+    return Product(
+      id: productModel.id,
+      name: productModel.name,
+      description: productModel.description,
+      price: productModel.price,
+      imageUrl: productModel.imageUrl,
+    );
+  }
+
+  static List<Product> toEntityList(List<ProductModel> productModels) {
+    return productModels.map((productModel) => toEntity(productModel)).toList();
+  }
+
+  static ProductModel toModel(Product product) {
+    return ProductModel(
+      id: product.id,
+      name: product.name,
+      description: product.description,
+      price: product.price,
+      imageUrl: product.imageUrl,
+    );
   }
 }
